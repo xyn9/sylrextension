e7251141bcee3eb75fa59d214d60e39713beab1a
xyn9 <xyn9.mail+github@gmail.com>
2011-02-06 09:27:38 +0900

    autoexec.nhk_news 0.91

--- autoexec.nhk_news.js
+++ autoexec.nhk_news.js
@@ -2,7 +2,7 @@
 //
 // ==UserScript==
 // @name autoexec.nhk_news
-// @version 0.9
+// @version 0.91
 // @include nhk.or.jp
 // @description NHKニュースの本文を自動で展開させる
 // @homepage http://xyn9.github.com/sylrextension
@@ -13,17 +13,24 @@
 //
 
 
-
-
 //
 (function (){
 	//
-	if( (/\/news\/[^\/]+$/i).test(location.pathname) ){
+	var $ID = '_autoexec_nhk_news';
+	//
+	if( (/\/news\/html\/\d+\/[a-z]\d+\.html$/i).test(location.pathname) ){
 		//
-		newsOpen();
+		if(document.getElementById($ID) == null){
+			document.getElementById('news_left').innerHTML += (
+				'<form id="'+ $ID +'">'
+				+ ('video id: <input type="text" name="video id:" value="' + document.getElementById('news_video').innerHTML + '" onclick="this.select()" /><br />')
+				+ ('short url: <input type="text" name="short url:" value="' + encodeShorturlNews(location.href) + '" onclick="this.select()" />')
+				+ '</form>'
+			)
+			;
+		}
 		//
 	}
-	//
 })();
 
 
